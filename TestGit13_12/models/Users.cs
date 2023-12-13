f9054dfb55a67d14b226b40221df9f086d071c6b
TD79 <anhboy311003@gmail.com>
2023-12-13 13:19:27 +0700

    Create Users.cs in folder models

---
+++ TestGit13_12/models/Users.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestGit13_12.models
+{
+    internal class Users
+    {
+    }
+}
